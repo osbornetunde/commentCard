1965e8ad3ecd2b015e91730988193223291d5842
Tunde Osborne <osbornetunde@gmail.com>
2019-02-19 00:30:45 +0100

    Updates

---
+++ precache-manifest.945763d1dba3d8e9ebeb4d999da0b0f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d11acff448f2488cf5a8",
+    "url": "/commentCard/static/js/runtime~main.d11acff4.js"
+  },
+  {
+    "revision": "9df53efee1a4a34f1cd5",
+    "url": "/commentCard/static/js/main.9df53efe.chunk.js"
+  },
+  {
+    "revision": "bc27f4d08ab001ab3e3d",
+    "url": "/commentCard/static/js/1.bc27f4d0.chunk.js"
+  },
+  {
+    "revision": "9df53efee1a4a34f1cd5",
+    "url": "/commentCard/static/css/main.469eb472.chunk.css"
+  },
+  {
+    "revision": "208751f5163f96ce1c9fc27ff3e7e909",
+    "url": "/commentCard/index.html"
+  }
+];
